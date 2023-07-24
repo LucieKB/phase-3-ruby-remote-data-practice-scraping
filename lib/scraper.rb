2eea77a09c4d417ccae9bf7ba59f2ecbe8e806ff
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-07-24 09:36:36 -0600

    commit tree 3f853a36cdb6a5936b3e727082fa3d43410fa3fe

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,19 @@
+# require 'nokogiri'
+# require 'open-uri'
+
+# html = open("https://flatironschool.com/")
+
+# uri = URI.open("https://flatironschool.com/blog/zachary-greenberg-musician-to-data-scientist/")
+# doc = Nokogiri::HTML(uri)
+# puts doc.css(".byline").text
+
+#post-5 > div > section.column-highlights.pt-sm.pb-sm.bg-white > div > div.mb-sm > header > h2
+
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".heading-primary")
+binding.pry
+
